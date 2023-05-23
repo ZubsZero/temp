89fee0ee9ff38be9aef577e9af66c5f8186cfc91
ZubsZero <zubairmatthee262@gmail.com>
2023-05-23 12:46:57 +0200

    Temp

---
+++ assets/JS/index.js
@@ -0,0 +1,16 @@
+const input = document.getElementById('input')
+const result = document.querySelector('results')
+const button = document.getElementById('button')
+const fahren = document.getElementById('Fahrenheight')
+
+
+
+function temperatureconverter(input) {
+    valNum = parseFloat(input);
+    document.getElementById("results").textContent=(valNum*1.8) + 32;
+  }
+
+  function calculate(fahren) {
+    valNum = parseFloat(fahren);
+    document.getElementById(" fh").textContent=(valNum-32)/1.8;
+  }
